86bf906279c559464a4e759cfdbcc71ab4cc8f1a
Almog Gabay <theninesky@gmail.com>
2020-01-09 22:41:19 +0200

    remove all cors-related definitions

--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ import express from 'express'
 import session from 'express-session'
 import mongoose from 'mongoose'
 import connectMongo from 'connect-mongo'
-// import cors from 'cors'
 
 import userRoutes from './routes/users'
 import noteRoutes from './routes/notes'
@@ -34,7 +33,6 @@ mongoose.connect(MONGODB_URI, {
 
 app.use(express.static(join(__dirname, '..', 'client', 'build')))
 app.use(express.json())
-// app.use(cors())
 
 app.use(session({
     cookie: { maxAge: +SESSION_LIFETIME, sameSite: true },
@@ -48,8 +46,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     res.setHeader('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS')
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
     next()
 });
 
