93dd24ac9e064462d9a6f7128132b6066765a83e
Almog Gabay <theninesky@gmail.com>
2020-03-29 08:49:07 +0300

    Replace os.cpus() with a single element array since clustering crashes Heroku

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import cluster from 'cluster'
-import os from 'os'
+// import os from 'os'
 import http from 'http'
 
 const { NODE_ENV, PORT = 5000, HEROKUAPP_URL = '' } = process.env
@@ -8,7 +8,8 @@ if (cluster.isMaster && NODE_ENV == 'production') {
   console.log(`Master is running...`)
 
   // eslint-disable-next-line
-  for (const _ of os.cpus()) {
+  for (const _ of new Array(1)) {
+    // os.cpus() crashes Heroku (Memory quota vastly exceeded)
     cluster.fork()
   }
 
