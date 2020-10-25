a7784e1627ce79c12862b441d05efc9d8d3bb6a6
Almog Gabay <theninesky@gmail.com>
2020-10-25 22:41:40 +0200

    Copy setup files from CRA 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import reportWebVitals from './reportWebVitals'
 
 import App from './components/App'
 import store from './store'
@@ -16,4 +17,9 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-serviceWorker.register()
+serviceWorkerRegistration.register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals(console.log)
