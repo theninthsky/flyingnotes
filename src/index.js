1cf7563c5f9ab7d3dbe2c96496b078f425790e88
Almog Gabay <theninesky@gmail.com>
2021-12-06 15:17:43 +0200

    Remove react-snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { hydrate, render } from 'react-dom'
+import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { getAuth, signOut } from 'firebase/auth'
 import { ErrorBoundary } from 'react-error-boundary'
@@ -11,14 +11,12 @@ import 'normalize.css'
 import 'styles/_globals.scss'
 
 const auth = getAuth(app)
-const rootElement = document.getElementById('root')
-const jsx = (
-  <BrowserRouter>
-  <ErrorBoundary fallback={<div>An error occured, please reload the app.</div>} onError={() => signOut(auth)}>
-    <App />
-  </ErrorBoundary>
-  </BrowserRouter>
-)
 
-if (rootElement.hasChildNodes()) hydrate(jsx, rootElement)
-else render(jsx, rootElement)
+render(
+  <BrowserRouter>
+    <ErrorBoundary fallback={<div>An error occured, please reload the app.</div>} onError={() => signOut(auth)}>
+      <App />
+    </ErrorBoundary>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
